98999af9781b32bf12b87f72bfdc9462cb56d0b7
Arif Ramdani <108125940+shelllbyyyyyy@users.noreply.github.com>
2025-04-12 08:11:00 +0700

    test: add unit test implmentation bcrypt hash password

---
+++ src/infrastructure/security/_test/BcryptPasswordHash.test.js
@@ -0,0 +1,20 @@
+import bcrypt from "bcryptjs";
+import BcryptPasswordHash from "../BcryptPasswordHash.js";
+
+describe("BcryptPasswordHash", () => {
+  describe("hash function", () => {
+    it("should encrypt password correctly", async () => {
+      // Arrange
+      const spyHash = jest.spyOn(bcrypt, "hash");
+      const bcryptPasswordHash = new BcryptPasswordHash(bcrypt);
+
+      // Action
+      const encryptedPassword = await bcryptPasswordHash.hash("plain_password");
+
+      // Assert
+      expect(typeof encryptedPassword).toEqual("string");
+      expect(encryptedPassword).not.toEqual("plain_password");
+      expect(spyHash).toHaveBeenCalledWith("plain_password", 10); // 10 adalah nilai saltRound default untuk BcryptPasswordHash
+    });
+  });
+});
